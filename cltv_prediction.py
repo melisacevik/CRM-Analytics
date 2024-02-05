e2ea8616b41f831066838ec07694fea670806abc
Melisa Çevik <celissamevik@gmail.com>
2024-02-05 15:03:18 +0300

    add CLTV Modelling

--- cltv_prediction.py
+++ cltv_prediction.py
@@ -159,3 +159,147 @@ cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
 cltv_df["recency"] = cltv_df["recency"] / 7
 cltv_df["T"] = cltv_df["T"] / 7
 cltv_df.describe().T
+
+##############################################################
+# 2. BG-NBD Modelinin Kurulması
+##############################################################
+
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+# bir model nesnesi oluşturacağım, bu nesne aracılığıyla sen fit metodunu kullanarak frequency, recency ve T değerlerini girdiğinde bu modeli kurar
+# penalizer_coef=0.001 => bu modelin parametrelerinin bulunması aşamasında katsayılara uygulanacak olan ceza katsayısıdır
+# bg nbd modeli bize en çok olabilirlik yöntemi ile beta ve gamma dağılımlarının parametrelerini bulmakta ve bir tahmin yapabilmemiz için ilgili
+# modeli oluşturmaktadır.
+
+bgf.fit(cltv_df['frequency'],
+        cltv_df['recency'],
+        cltv_df['T'])
+
+################################################################
+# 1 hafta içinde en çok satın alma beklediğimiz 10 müşteri kimdir?
+################################################################
+
+bgf.conditional_expected_number_of_purchases_up_to_time(1,
+                                                        cltv_df['frequency'],
+                                                        cltv_df['recency'],
+                                                        cltv_df['T']).sort_values(ascending=False).head(10)
+
+# con_... => bgf nin bir fonksiyonu
+# t: 1 => 1 haftalık tahmin yap
+
+# bir hafta içinde beklediğimiz satın almaları hesaplayıp bunu cltv_df ' e ekle
+
+cltv_df["expected_purc_1_week"] = bgf.predict(1,
+                                              cltv_df['frequency'],
+                                              cltv_df['recency'],
+                                              cltv_df['T'])
+
+# buradaki predict ve conti_... olan fonk. aynı işlemi görür fakat gamma'da predict kullanılmaz!! o yüzden cont_.. daha iyi
+
+################################################################
+# 1 ay içinde en çok satın alma beklediğimiz 10 müşteri kimdir?
+################################################################
+# t => 4 bir aylık tahmin
+
+cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+# 1 ay içerisinde ne kadar satın alma olabilecek?
+
+bgf.predict(4,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()  # bir aylık periyotta şirketimizin beklediği satış sayısı budur
+
+################################################################
+# 3 Ayda Tüm Şirketin Beklenen Satış Sayısı Nedir?
+################################################################
+
+bgf.predict(4 * 3,
+            cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T']).sum()
+
+cltv_df["expected_purc_3_month"] = bgf.predict(4 * 3,
+                                               cltv_df['frequency'],
+                                               cltv_df['recency'],
+                                               cltv_df['T'])
+
+################################################################
+# Tahmin Sonuçlarının Değerlendirilmesi
+################################################################
+
+# burada öncelikle basit bir grafik üzerinden tahmin sonuçlarının başarısı değerlendirilebilir.
+plot_period_transactions(bgf)
+plt.show()
+
+# gerçek değerler maviler , tahmin edilenler turuncular ( modelin tahmin ettiği )
+
+
+# bg/nbd => satın alma sayısını modeller
+
+##############################################################
+# 3. GAMMA-GAMMA Modelinin Kurulması
+##############################################################
+
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+
+ggf.fit(cltv_df['frequency'], cltv_df['monetary'])  # model kur bilgisi
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],  # toplam işlem sayısı
+                                        cltv_df['monetary']).head(10)  # işlem başına ort değeri
+
+# azalan istersek
+
+ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                        cltv_df['monetary']).sort_values(ascending=False).head(10)
+# bütün müşterilerin içerisinde beklenen karı, expected average ı, ortalama karı getirmiş oldu
+
+cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                             cltv_df['monetary'])
+
+cltv_df.sort_values("expected_average_profit", ascending=False).head(10)
+
+# potansiyel müşteri değerlerini de yakalama kabiliyeti var
+
+# elimizde, expected transaction değerleri var,1 haftalık , 1 aylık , 3 aylık , ortalama beklenen karlılık değerleri müşteri özelinde hesaplandı.
+
+##############################################################
+# 4. BG-NBD ve GG modeli ile CLTV'nin hesaplanması.
+##############################################################
+
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency'],
+                                   cltv_df['T'],
+                                   cltv_df['monetary'],
+                                   time=3,  # 3 aylık
+                                   freq="W",  # T'nin frekans bilgisi.
+                                   discount_rate=0.01)
+
+cltv.head()
+
+cltv = cltv.reset_index()
+# cltv fonksiyonu gammagamma ve bgnbd modelini ver,frequ,recency,yaş, monetary göster, zaman periyodu ver, ( time = aylık ) , (T ve recency) girdiğin değerler haftalıksa frekans ver W
+# zaman içerisinde sattığın ürünlerde çeşitli indirimler yapabilirsin. bunu da göz önünde bulundur. => discount_rate = 0.01
+
+# daha önce cltv_df oluşturmuştuk , cltv_df ile cltv leri ifade eden veri setini birleştirmemiz gerekiyor.
+
+cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")  # cltv_df'e göre birleştir
+cltv_final.sort_values(by="clv", ascending=False).head(10)
+
+# clv ne ? return ettiği df'deki isimlendirme
+# inceleyelim en üsttekini
+# bu kişinin 3 aylık periyotta yapması beklenen satış sayısı 14 birim.
+
+# örneğin 1257 id ye sahip müşteriye bak,
+# gözlemlerde en fazla satın alma buradan beklenirken ( expected_purc_3_month) , karlılığı düşük olduğu ( expected_avg_ profit ) için en yukarda değil.!
+# karlılığın düşük olduğunu monetaryden de bakabilirsin. bu kişi yüksek frekansta alışveriş yapmış ama düşük hacimli yapmış
+
+
+# recency'si kendi içinde bu kadar yüksek olan bu müşteriler nasıl cltv'de en büyük değeri vadediyor?
+# senin için düzenli olan bir müşterinin kendi içinde recency değeri arttıkça, müşterinin satın alma olasılığı yaklaşıyordur
+# bu veri seti özelinde, recency ve T değeri birbirine yakında yüksek CLTV değerleri elde edildi.
+# ilk 5 e baktığımda frequency değeri düşük olan müşteri var. monetary yüksek mi ? evet. o zaman bu potansiyel müşteri anlamına gelir.
+# hepsine bakıp yorumlamak önemli
