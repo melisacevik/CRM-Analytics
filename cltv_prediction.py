64ae49dbb2fde92dcc6dafc0bc951b50053fb117
Melisa Çevik <celissamevik@gmail.com>
2024-02-05 15:16:25 +0300

    add CLTV function and segments

--- cltv_prediction.py
+++ cltv_prediction.py
@@ -303,3 +303,97 @@ cltv_final.sort_values(by="clv", ascending=False).head(10)
 # bu veri seti özelinde, recency ve T değeri birbirine yakında yüksek CLTV değerleri elde edildi.
 # ilk 5 e baktığımda frequency değeri düşük olan müşteri var. monetary yüksek mi ? evet. o zaman bu potansiyel müşteri anlamına gelir.
 # hepsine bakıp yorumlamak önemli
+
+
+##############################################################
+# 5. CLTV'ye Göre Segmentlerin Oluşturulması
+##############################################################
+
+cltv_final
+
+cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["D", "C", "B", "A"])
+
+cltv_final.sort_values(by="clv", ascending=False).head(50)
+
+cltv_final.groupby("segment").agg(
+    {"count", "mean", "sum"})
+
+
+##############################################################
+# 6. Çalışmanın Fonksiyonlaştırılması
+##############################################################
+
+# asıl amaç cltv hesaplamak olduğu için month parametresi girdik
+def create_cltv_p(dataframe, month=3):
+    # 1. Veri Ön İşleme
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    today_date = dt.datetime(2011, 12, 11)
+
+    cltv_df = dataframe.groupby('Customer ID').agg(
+        {'InvoiceDate': [lambda InvoiceDate: (InvoiceDate.max() - InvoiceDate.min()).days,
+                         lambda InvoiceDate: (today_date - InvoiceDate.min()).days],
+         'Invoice': lambda Invoice: Invoice.nunique(),
+         'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    cltv_df.columns = cltv_df.columns.droplevel(0)
+    cltv_df.columns = ['recency', 'T', 'frequency', 'monetary']
+    cltv_df["monetary"] = cltv_df["monetary"] / cltv_df["frequency"]
+    cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+    cltv_df["recency"] = cltv_df["recency"] / 7
+    cltv_df["T"] = cltv_df["T"] / 7
+
+    # 2. BG-NBD Modelinin Kurulması
+    bgf = BetaGeoFitter(penalizer_coef=0.001)
+    bgf.fit(cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T'])
+
+    cltv_df["expected_purc_1_week"] = bgf.predict(1,
+                                                  cltv_df['frequency'],
+                                                  cltv_df['recency'],
+                                                  cltv_df['T'])
+
+    cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_3_month"] = bgf.predict(12,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    # 3. GAMMA-GAMMA Modelinin Kurulması
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(cltv_df['frequency'], cltv_df['monetary'])
+    cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                                 cltv_df['monetary'])
+
+    # 4. BG-NBD ve GG modeli ile CLTV'nin hesaplanması.
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency'],
+                                       cltv_df['T'],
+                                       cltv_df['monetary'],
+                                       time=month,  # 3 aylık
+                                       freq="W",  # T'nin frekans bilgisi.
+                                       discount_rate=0.01)
+
+    cltv = cltv.reset_index()
+    cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+    cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["D", "C", "B", "A"])
+
+    return cltv_final
+
+
+df = df_.copy()
+
+cltv_final2 = create_cltv_p(df)
+
+cltv_final2.to_csv("cltv_prediction.csv")
